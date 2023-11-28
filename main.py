d07a9f9afbaba156890bbbdf7dfcacddb7a99af5
DrNareg <122312987+DrNareg@users.noreply.github.com>
2023-11-28 02:10:56 +0000

    improved data structure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-category_weights = {"Attendance":0, "Homework":0, "Midterm_1":0, "Midterm_2":0, "Final":0}
-category_grades = {"Attendance":0, "Homework":0, "Midterm_1":0, "Midterm_2":0, "Final":0}
+category_weights = {"Attendance":[0,0], "Homework":[0,0]}
 symbol = "%"
 
 # Just a simple intro to the program
@@ -7,25 +6,31 @@ def intro():
     print("So you're here to check how screwed you are. lol")
     print("Just a few notes:\nNo need for " + symbol+ " sign \nEnter 0 for any un-needed categories\n")
 
-def collect_weights():
+# Function to collect important data for the dictionary
+def collect_data():
 
-    # Loop through and update the dictionary
+    # Loop through and update the dictionary with weights
     for category in category_weights:
-        # get input
+        
+        # get weight
         weight = input(f"Enter weight for " + category + ': ')
-
         # check if input includes %
         while symbol in weight:
             print("Bruh, I said no %")
-            weight = input(f"Enter weight for " + category + ': ')
+            weight = input(f"Enter weight for " + category + ': ')   
+
+        # Get grade
+        grade = input(f"Enter current grade for " + category + ': ')
+        # check if input includes %
+        while symbol in grade:
+            print("Bruh, I said no %")
+            grade = input(f"Enter grade for " + category + ': ')
         
         # Update the dictionary values
-        category_weights[category] = weight
+        category_weights[category][0] = weight
+        category_weights[category][1] = grade
 
-    print(category_weights)
+    #print(category_weights)
 
-def collect_grades():
-    print()
 
-intro()
-#collect_weights()
+collect_data()
